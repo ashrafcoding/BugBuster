2a9cadd1020c92f31a5a2786ffda2832e8d10f7a
ashraf <ashrafeid@outlook.com>
2023-11-13 21:33:59 +0300

    add navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import { ModeToggle } from "./ModeToggle"
+
+
+export function Navbar() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 p-3 flex justify-between w-full border-b ">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Image src="/logo.png" width={50} height={50} alt="logo" className="rounded-full"/>
+        <span className="hidden font-bold text-xl sm:inline-block">
+          Bug Buster
+        </span>
+      </Link>
+      <nav className="flex items-center space-x-6 text-sm font-medium">
+        <Link
+          href="/docs"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/docs" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Documentation
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/docs/components")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Components
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/themes")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Themes
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/examples")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Examples
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
+          )}
+        >
+          GitHub
+        </Link>
+
+      </nav>
+      <ModeToggle/>
+    </div>
+  )
+}
